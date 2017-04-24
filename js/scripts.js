7747569cbaf394901d29fde28c2589565dbca8d2
buna ezeofor and kimlan nguyen <kimlan@>
2017-04-24 10:46:03 -0700

    add a lot of converters.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,14 @@ var add = function(number1, number2){return number1 + number2;};
 var sub = function(number1, number2){return number1 - number2;};
 var mul = function(number1, number2) {return number1 * number2;};
 var div = function(number1, number2){return number1 / number2;};
-var number1 = parseInt(prompt("enter a number"));
+var bmi = function(weight, height){return (weight*703 / (height * height));};
+var C_to_F = function(temp){return (temp*1.8+32);};
+var gal_to_lit = function(gal){return(gal*3.78541);};
+/*var number1 = parseInt(prompt("enter a number"));
 var number2 = parseInt(prompt("enter another number"));
-var result = mul(number1, number2);
+var weight = parseInt(prompt("enter your weight in lbs"));
+var height = parseInt(prompt("enter your height in inches"));*/
+var gal = parseInt(prompt("enter a volume in gallon"));
+var result = gal_to_lit(gal);
 
 alert(result);
