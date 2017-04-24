32d5e0332a95525ba68d158accc6b2943e41d8e5
buna ezeofor and kimlan nguyen <buna@>
2017-04-24 10:23:37 -0700

    create a calculator using JavaScript.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add = function(number1, number2){return number1 + number2;};
+var sub = function(number1, number2){return number1 - number2;};
+var mul = function(number1, number2) {return number1 * number2;};
+var div = function(number1, number2){return number1 / number2;};
+var number1 = parseInt(prompt("enter a number"));
+var number2 = parseInt(prompt("enter another number"));
+var result = mul(number1, number2);
+
+alert(result);
